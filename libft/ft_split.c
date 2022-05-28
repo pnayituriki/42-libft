605fc77b3a65ec1c2d713f87c9418dc2c7ec9b4f
pnayitur <pnayituriki@gmail.com>
2022-05-28 17:02:13 +0200

    split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,55 +6,86 @@
 /*   By: pnayitur <pnayitur@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 23:12:57 by pnayitur          #+#    #+#             */
-/*   Updated: 2022/05/28 14:10:42 by pnayitur         ###   ########.fr       */
+/*   Updated: 2022/05/28 17:00:48 by pnayitur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_wordcount(char const *s, char c)
+static size_t	ft_count_words(char const *str, char c)
 {
-	size_t	i;
 	size_t	words;
+	size_t	i;
 
-	i = 0;
 	words = 0;
-	if (s == 0 || s[0] == '\0')
-		return (0);
-	while (s[i] != 0)
+	i = 0;
+	while (str[i] != '\0')
 	{
-		if (s[i] != c && (i == 0 || s[i - 1] == c))
+		while (str[i] == c)
+			i++;
+		if (str[i] != c && str[i] != '\0')
 			words++;
-		i++;
+		while (str[i] != c && str[i] != '\0')
+			i++;
 	}
 	return (words);
 }
 
+static size_t	ft_counts_len_word(char const *str, char c)
+{
+	size_t	count;
+	size_t	i;
+
+	count = 0;
+	i = 0;
+	while (str[i] == c)
+		i++;
+	while (str[i] != c && str[i] != '\0')
+	{
+		count++;
+		i++;
+	}
+	return (count);
+}
+
+static void	ft_free_memory(char **array)
+{
+	size_t	i;
+
+	i = 0;
+	while (array[i])
+	{
+		free(array[i]);
+		i++;
+	}
+	free(array);
+}
+
 char	**ft_split(char const *s, char c)
 {
-	char	**arr;
-	int		i;
-	int		j;
-	int		start;
+	size_t i;
+	size_t words;
+	char **array;
 
-	arr = malloc(sizeof(char *) * (ft_wordcount(s, c) + 1));
-	if (!arr || !s)
+	if (!s)
 		return (NULL);
-	j = 0;
 	i = 0;
-	while (s[i])
+	words = ft_count_words((char *)s, c);
+	array = (char **)malloc(sizeof(char *) * (words + 1));
+	if (!array)
+		return (NULL);
+	while (i < words)
 	{
-		while (s[i] == c)
-			i++;
-		start = i;
-		while (s[i] && s[i] != c)
-			i++;
-		if (start != i)
+		while (*s == c)
+			s++;
+		array[i] = ft_substr(s, 0, ft_counts_len_word(s, c));
+		if (!array[i++])
 		{
-			arr[j] = ft_substr(s, start, i - start);
-			j++;
+			ft_free_memory(array);
+			return (NULL);
 		}
+		s += ft_counts_len_word(s, c);
 	}
-	arr[j] = 0;
-	return (arr);
+	array[i] = NULL;
+	return (array);
 }
