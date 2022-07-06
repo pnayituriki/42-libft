f70ffac5d56b2b8065077f676486ccc4b3830bf6
pnayitur <pnayituriki@gmail.com>
2022-07-06 12:03:47 +0200

    changes

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,49 +6,78 @@
 /*   By: pnayitur <pnayitur@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 17:09:09 by pnayitur          #+#    #+#             */
-/*   Updated: 2022/06/23 12:10:54 by pnayitur         ###   ########.fr       */
+/*   Updated: 2022/07/06 11:55:01 by pnayitur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_get_size(int n)
+static char	*ft_zero_case(int n)
 {
-	int	len;
+	char	*str;
 
-	len = 0;
-	if (n <= 0)
-		len = 1;
+	str = malloc(2);
+	if (!str)
+		return (NULL);
+	str[n] = 48;
+	str[1] = '\0';
+	return (str);
+}
+
+static int	ft_alen(int n, int j)
+{
 	while (n != 0)
 	{
 		n = n / 10;
-		len++;
+		j++;
 	}
-	return (len);
+	return (j);
 }
 
-char	*ft_itoa(int num)
+static char	*ft_logic(char *str, int m, int j, int i)
 {
-	char	*str_num;
-	size_t	digits;
-
-	digits = ft_get_size(num);
-	str_num = (char *)malloc(sizeof(char) * (digits + 1));
-	if (!str_num)
-		return (0);
-	str_num[digits] = '\0';
-	if (num == 0)
-		str_num[0] = '0';
-	while (num != 0 && digits > 0)
+	if (m == -2147483648)
 	{
-		if (num < 0)
-		{
-			num = num * -1;
-			str_num[0] = '-';
-		}
-		str_num[digits - 1] = (num % 10) + '0';
-		num = num / 10;
-		digits--;
+		m = -214748364;
+		str[j] = (8 + 48);
+		j--;
 	}
-	return (str_num);
+	if (m > -2147483648 && m < 0)
+		m = i * m;
+	while (m != 0 && j >= 0)
+	{
+		str[j] = ((char)((m % 10)) + 48);
+		m = m / 10;
+		j--;
+	}
+	if (i < 0)
+		str[0] = '-';
+	return (str);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	int		i;
+	int		j;
+	int		m;
+
+	i = 0;
+	j = 0;
+	m = n;
+	if (n == 0)
+		return (ft_zero_case(n));
+	j = ft_alen(n, j);
+	if (m < 0)
+	{
+		j++;
+		i = -1;
+	}
+	str = malloc(1 * (j + 1));
+	if (!str)
+		return (NULL);
+	str[j] = '\0';
+	j--;
+	str = ft_logic(str, m, j, i);
+	return (str);
 }
